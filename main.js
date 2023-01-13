2ad2d9c581cb8d11c8f1d5026ce99751ed0d29e8
LuisC95 <luiscarmona.95@gmail.com>
2023-01-13 18:47:42 -0500

    first try

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
 const grace = new Baby("Grace", "Female", 7.11, 19.5);
 
-console.log(grace)
+let millisec = 0;
+let sec = 0; 
+let min = 0;
+let hours = 0;
+
+ function chrono()
+ {
+    millisec++
+    if(millisec > 99)
+    {
+        millisec = 0;
+        sec++
+        if(sec > 60)
+        {
+            sec = 0;
+            min++
+            if(min = 5)
+            {
+                millisec = 'l';
+            }
+        }
+    }
+    console.log(min, sec, millisec)
+ }
+
+ chrono();
+
+console.log(millisec, sec, min, hours)
